8173c736b48da7a7e1c83a131415f0d45f2cbf98
ridget <tom.ridge@tworedkites.com>
2014-08-25 15:14:02 +1000

    Initial commit

---
+++ config/deploy/integ.rb
@@ -0,0 +1,3 @@
+set :rails_env, 'integ'
+set :branch, 'develop'
+#server 'APP_NAME.integ.tworedkites.com', :web, :app, :db, primary: true
---
+++ config/deploy/uat.rb
@@ -0,0 +1,3 @@
+set :rails_env, 'uat'
+set :branch, 'develop'
+#server 'APP_NAME.uat.tworedkites.com', :web, :app, :db, primary: true
